441ac911423936965eb93059d246d926fb66c4f5
vascofidanza <vasco.fidanza@gmail.com>
2023-04-30 20:45:33 +0100

    Criei a view para remover um produto

--- store/urls.py
+++ store/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('editProfile_view/', views.edit_profile, name="edit_profile"),
     path('redirectDeleteStaff/', views.redirectDeleteStaff, name='redirectDeleteStaff'),
     path('delete_staff/<int:staff_id>/', views.delete_staff, name='delete_staff'),
+    path('removeProduct/', views.removeProduct, name='removeProduct'),
+    path('remove_Product', views.redirectRemoveProduct, name='redirectRemoveProduct'),
 
 ]
